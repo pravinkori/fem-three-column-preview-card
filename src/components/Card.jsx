6ddbff8a0effba7ac9014599ad45b8203be5fe63
Pravin Kori <pravinkori@protonmail.ch>
2024-02-17 19:14:05 +0530

    chore: update card component and remove unused Credits component
    The changes in this commit update the Card component in the client application. The changes include adding images and descriptions for different types of cars (sedans, SUVs, and luxury cars) and adding a button to learn more about each type of car. Additionally, the unused Credits component has been commented out. These changes improve the visual presentation and user experience of the application.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,59 @@
 /* eslint-disable react/prop-types */
+import "../css/App.css";
+import sedanIcon from "../assets/images/icon-sedans.svg";
+import suvIcon from "../assets/images/icon-suvs.svg";
+import luxuryIcon from "../assets/images/icon-luxury.svg";
 
 function Card() {
-  return <h1>Hello</h1>;
+  return (
+    <article className="card flex">
+      <section className="sedans">
+        <picture>
+          <img src={sedanIcon} alt="Sedan icon" />
+        </picture>
+        <div className="card__content">
+          <h1 className="card__title">sedans</h1>
+          <div className="card__description">
+            <p>
+              Choose a sedan for its affordability and excellent fuel economy.
+              Ideal for cruising in the city or on your next road trip.
+            </p>
+          </div>
+          <button className="button">Learn More</button>
+        </div>
+      </section>
+      <section className="suvs">
+        <picture>
+          <img src={suvIcon} alt="Suv icon" />
+        </picture>
+        <div className="card__content">
+          <h1 className="card__title">suvs</h1>
+          <div className="card__description">
+            <p>
+              Take an SUV for its spacious interior, power, and versatility.
+              Perfect for your next family vacation and off-road adventures.
+            </p>
+          </div>
+          <button className="button">Learn More</button>
+        </div>
+      </section>
+      <section className="luxury">
+        <picture>
+          <img src={luxuryIcon} alt="luxury icon" />
+        </picture>
+        <div className="card__content">
+          <h1 className="card__title">luxury</h1>
+          <div className="card__description">
+            <p>
+              Cruise in the best car brands without the bloated prices. Enjoy
+              the enhanced comfort of a luxury rental and arrive in style.
+            </p>
+          </div>
+          <button className="button">Learn More</button>
+        </div>
+      </section>
+    </article>
+  );
 }
 
 export default Card;
