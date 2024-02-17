867eb93d042770261bee91d0155ae98cd20e8ba5
Pravin Kori <pravinkori@protonmail.ch>
2024-02-17 17:53:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card";
+import Credits from "./components/Credits";
+import "./css/App.css";
+
+function App() {
+  return (
+    <>
+      <Card />
+      <Credits />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+
+function Card() {
+  return <h1>Hello</h1>;
+}
+
+export default Card;
