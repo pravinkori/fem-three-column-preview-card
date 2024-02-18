a552cb0003077b3eed4924b40efe43a191b0769c
Pravin Kori <pravinkori@protonmail.ch>
2024-02-18 13:47:16 +0530

    chore: update card styling and layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,53 +6,49 @@ import luxuryIcon from "../assets/images/icon-luxury.svg";
 
 function Card() {
   return (
-    <article className="card">
-      <section className="sedans">
-        <div className="card__content">
+    <main>
+      <section className="card__container">
+        {/* Sedan */}
+        <div className="card__sedan">
           <picture>
             <img src={sedanIcon} alt="Sedan icon" />
           </picture>
           <h1 className="card__title">sedans</h1>
-          <div className="card__description">
-            <p>
-              Choose a sedan for its affordability and excellent fuel economy.
-              Ideal for cruising in the city or on your next road trip.
-            </p>
-          </div>
-          <button className="button">Learn More</button>
+          <p className="card__description">
+            Choose a sedan for its affordability and excellent fuel economy.
+            Ideal for cruising in the city or on your next road trip.
+          </p>
+          <a className="btn">Learn More</a>
+          {/* <button className="button">Learn More</button> */}
         </div>
-      </section>
-      <section className="suvs">
-        <div className="card__content">
+
+        {/* Suv */}
+        <div className="card__suv">
           <picture>
-            <img src={suvIcon} alt="Suv icon" />
+            <img src={suvIcon} alt="suv icon" />
           </picture>
           <h1 className="card__title">suvs</h1>
-          <div className="card__description">
-            <p>
-              Take an SUV for its spacious interior, power, and versatility.
-              Perfect for your next family vacation and off-road adventures.
-            </p>
-          </div>
-          <button className="button">Learn More</button>
+          <p className="card__description">
+            Take an SUV for its spacious interior, power, and versatility.
+            Perfect for your next family vacation and off-road adventures.
+          </p>
+          <a className="btn">Learn More</a>
         </div>
-      </section>
-      <section className="luxury">
-        <div className="card__content">
+
+        {/* Luxury */}
+        <div className="card__luxury">
           <picture>
             <img src={luxuryIcon} alt="luxury icon" />
           </picture>
           <h1 className="card__title">luxury</h1>
-          <div className="card__description">
-            <p>
-              Cruise in the best car brands without the bloated prices. Enjoy
-              the enhanced comfort of a luxury rental and arrive in style.
-            </p>
-          </div>
-          <button className="button">Learn More</button>
+          <p className="card__description">
+            Cruise in the best car brands without the bloated prices. Enjoy the
+            enhanced comfort of a luxury rental and arrive in style.
+          </p>
+          <a className="btn">Learn More</a>
         </div>
       </section>
-    </article>
+    </main>
   );
 }
 
