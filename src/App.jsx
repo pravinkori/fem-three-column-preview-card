47fdd80b38593704cf167b5c88add75b2fb23ef8
Pravin Kori <pravinkori@protonmail.ch>
2024-02-19 11:54:36 +0530

    feat: update card data source to import from separate file
    The card data in the App component was previously hardcoded in the component itself. This commit updates the data source to import the card data from a separate file called "cardData.js". This change allows for easier management and modification of the card data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
+import cardData from "./constants/cardData";
 import Card from "./components/Card";
-import sedanIcon from "./assets/images/icon-sedans.svg";
-import suvIcon from "./assets/images/icon-suvs.svg";
-import luxuryIcon from "./assets/images/icon-luxury.svg";
 import "./css/App.css";
-
-const cardsData = [
-  {
-    icon: sedanIcon,
-    title: "Sedan",
-    description:
-      "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
-  },
-  {
-    icon: suvIcon,
-    title: "SUV",
-    description:
-      "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
-  },
-  {
-    icon: luxuryIcon,
-    title: "Luxury",
-    description:
-      "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
-  },
-];
 
 function App() {
   return (
     <main>
       <section className="card__container">
-        {cardsData.map((card, index) => (
+        {cardData.map((card, index) => (
           <Card
             key={index}
             icon={card.icon}
